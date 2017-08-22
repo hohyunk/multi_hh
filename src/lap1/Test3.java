1026e4af763f00bd4bf6d90ffc87a97194a4357c
M100124\student <fcs@nate.com>
2017-08-22 09:27:37 +0900

    세번째 버전.

--- src/lap1/Test3.java
+++ src/lap1/Test3.java
@@ -3,7 +3,8 @@ package lap1;
 public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("뭐야이거ㅡㅡ");
+		System.out.println("뭐야이거");
+		System.out.println("개판이야 개판");
 	}
-
+	
 }
